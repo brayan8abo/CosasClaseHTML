46ca30ceaca8428ee5837d5de1b35f4b7e5d17d4
Brayan8abo <brayan8abo@outlook.es>
2024-03-19 19:53:45 +0100

    udapte

--- comandos-Windows/borrar.bat
+++ comandos-Windows/borrar.bat
@@ -1,26 +1,12 @@
 @echo OFF
 
-rem if donde comparo si esta
-if "%1" == "dir_help" echo primer parametro es dir_help
-if not "%1" == "dir_help3" echo primer parametro NO es dir_help
-
-if exist %1 del %1
-if exist %2 del %2
-
-
 rem elimino el primero y en el echo le muestro el mensaje
 
-if exist %1 echo %1 existe
+del %1
+echo %1 borrado
 
-
-
-rem del %1
-rem if not exist %1 echo %1 no existe
-rem echo %1 borrado
-
-rem pause para pedir continuacion, con una tecla > null no saca mensaje de confirmacion
-
-rem pause
-rem echo %2
-rem del %2 
-rem echo %2 borrado
+rem pause para pedir continuacion, con una tecla
+pause
+echo %2
+del %2 
+echo %2 borrado
